002d512ade914e14f39d3edbc14530b48f5ceb6c
Humdinger <humdinger@mailbox.org>
2024-07-22 17:39:36 +0200

    Reconcile window: alternate tinting of list items
    Similar to the transactions list of the main window.

--- src/ReconcileItem.cpp
+++ src/ReconcileItem.cpp
@@ -1,11 +1,15 @@
 #include "ReconcileItem.h"
+
 #include <ListView.h>
 #include <Region.h>
 #include <View.h>
+
 #include <stdio.h>
 #include <ctime>
+
 #include "CBLocale.h"
 #include "Database.h"
+#include "Preferences.h"
 #include "TransactionData.h"
 
 
@@ -21,17 +25,31 @@ ReconcileItem::~ReconcileItem(void) {}
 void
 ReconcileItem::DrawItem(BView* owner, BRect frame, bool complete)
 {
-	owner->SetHighUIColor(
-		IsSelected() ? B_LIST_SELECTED_BACKGROUND_COLOR : B_LIST_BACKGROUND_COLOR);
-	owner->FillRect(frame);
+	BListView* list = (BListView*)owner;
+	int32 index = list->IndexOf(this);
 
 	if (IsSelected()) {
+		owner->SetHighUIColor(B_LIST_SELECTED_BACKGROUND_COLOR);
+		owner->FillRect(frame);
 		owner->SetHighUIColor(B_CONTROL_HIGHLIGHT_COLOR);
 		owner->StrokeRect(frame);
+	} else {
+		if (index % 2 == 1) {  // darken odd row
+			owner->SetHighUIColor(B_LIST_BACKGROUND_COLOR,
+				GetMutedTint(ui_color(B_LIST_BACKGROUND_COLOR), CB_ALT_ROW));
+		} else
+			owner->SetHighUIColor(B_LIST_BACKGROUND_COLOR);
+
+		owner->FillRect(frame);
 	}
 
 	owner->SetFont(IsReconciled() ? be_bold_font : be_plain_font);
-	owner->SetHighUIColor(B_LIST_ITEM_TEXT_COLOR);
+	owner->SetHighUIColor(IsSelected() ? B_LIST_SELECTED_ITEM_TEXT_COLOR : B_LIST_ITEM_TEXT_COLOR);
+
+	// Compute vertical alignment factor
+	font_height fh;
+	owner->GetFontHeight(&fh);
+	float fontFactor = ceilf(fh.ascent + fh.descent + fh.leading) / 4 + 1;
 
 	// Draw amount first
 	BString string;
@@ -42,15 +60,14 @@ ReconcileItem::DrawItem(BView* owner, BRect frame, bool complete)
 		gCurrentLocale.CurrencyToString(fTransaction.Amount(), string);
 
 	float width = owner->StringWidth(string.String());
-	owner->DrawString(string.String(), BPoint(frame.right - width, frame.bottom));
-
+	owner->DrawString(string.String(), BPoint(frame.right - width - 5, frame.bottom - fontFactor));
 
 	// Draw Payee next
 	BRect r(frame);
 	r.right -= width + 5;
 	BRegion region(r);
 	owner->ConstrainClippingRegion(&region);
-	owner->DrawString(fTransaction.Payee(), BPoint(frame.left + 1, frame.bottom - 2));
+	owner->DrawString(fTransaction.Payee(), BPoint(frame.left + 5, frame.bottom - fontFactor));
 	owner->ConstrainClippingRegion(NULL);
 }
 
