4c70ab5c48fa3ddca6baeffb8b147ac5ca199f99
Humdinger <humdinger@mailbox.org>
2024-07-27 11:31:31 +0200

    Add context menu to "Scheduled Transactions" window
    Since the user might expect it now that the other lists have it...

--- src/ScheduleListWindow.h
+++ src/ScheduleListWindow.h
@@ -1,7 +1,46 @@
 #ifndef SCHEDULELIST_WINDOW
 #define SCHEDULELIST_WINDOW
 
+#include "ColumnListView.h"
+#include <PopUpMenu.h>
 #include <Window.h>
+
+enum {
+	M_REMOVE_ITEM = 'rmit',
+	M_SELECTION,
+	M_CLOSE_CONTEXT
+};
+
+
+class ScheduleContext : public BPopUpMenu {
+public:
+	ScheduleContext(const char* name, BMessenger target);
+	virtual ~ScheduleContext();
+
+private:
+	BMessenger fTarget;
+};
+
+
+class ScheduleListView : public BView {
+public:
+	ScheduleListView(const char* name, const int32& flags);
+	void AttachedToWindow(void);
+	void MessageReceived(BMessage* msg);
+
+private:
+	// This is a float so we can get the maximum string width for payees.
+	float RefreshScheduleList(void);
+
+	BColumnListView* fListView;
+	void ShowPopUpMenu(BPoint screen);
+	bool fShowingPopUpMenu;
+
+	BButton* fRemoveButton;
+	BList fTransList;
+
+	float fBestWidth;
+};
 
 class ScheduleListWindow : public BWindow {
 public:
